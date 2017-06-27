08c049a3604a7adfadb30aed4beed87559fdef4e
方雨_Yu <304683191@qq.com>
2017-06-27 23:54:30 +0800

    增加 退出功能 与 全局是否登录状态

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ App({
 	globalData: {
 		userInfo: null,
 		auth: null,
+		isLogin: false,
 	},
 	onLaunch () {
 		//调用API
@@ -11,20 +12,34 @@ App({
 	// 获取 Auth
 	getAuth() {
 		try {
-			this.globalData.auth = wx.getStorageSync('auth');
+			this.globalData.auth = wx.getStorageSync('auth') || null;
 			console.log('当前 auth => ', this.globalData.auth);
 		} catch (err) {
 			console.log(err);
 		}
 	},
-	// 设置 Auth
+	// 设置 Auth - 登录
 	setAuth(auth) {
 		try {
 			wx.setStorageSync('auth', auth);
 			this.globalData.auth = auth;
+			this.globalData.isLogin = true;
 			console.log('当前 auth => ', this.globalData.auth);
 		} catch (err) {
 			console.log(err);
 		}
 	},
+	// 删除 Auth - 退出登录
+	removeAuth() {
+		try {
+			wx.removeStorageSync('auth');
+			this.globalData.auth = null;
+			this.globalData.userInfo = null;
+			this.globalData.isLogin = false;
+			console.log('当前 auth => ', this.globalData.auth);
+			console.log('当前 userInfo => ', this.globalData.userInfo);
+		} catch (err) {
+			console.log(err);
+		}
+	}
 })
